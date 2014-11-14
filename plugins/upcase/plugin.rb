1c72c429372ea1ffed7a6fcd6d965026a57cdb2e
Chad Pytel <chad@thoughtbot.com>
2014-11-14 15:57:55 -0500

    Revert "plugin to allow embedding"
    This reverts commit 8eff04b40378dd523dae1d3851936e42a6a91dd1.

--- plugins/upcase/plugin.rb
+++
@@ -1,6 +0,0 @@
-# name: Upcase customizations
-# about: customizations for Upcase forum
-# version: 1
-# authors: thoughtbot
-
-Rails.application.config.action_dispatch.default_headers.merge!({'X-Frame-Options' => 'ALLOWALL'})
