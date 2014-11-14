43760e203cb1d9f2174d9066b889eb7a7fbdf88e
Chad Pytel <chad@thoughtbot.com>
2014-11-14 14:09:03 -0500

    print the caller of rescued actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -116,8 +116,7 @@ class ApplicationController < ActionController::Base
   end
 
   def rescue_discourse_actions(message, error, include_ember=false)
-    p error
-    p error.backtrace
+    p caller
     if request.format && request.format.json?
       # TODO: this doesn't make sense. Stuffing an html page into a json response will cause
       #       $.parseJSON to fail in the browser. Also returning text like "[error: 'invalid access']"
